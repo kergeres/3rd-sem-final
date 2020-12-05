6150040ac47b36435ccbacb012d2623b7d63922f
kergeres <55786735+kergeres@users.noreply.github.com>
2020-12-05 11:20:35 +0100

    auth0

--- script/main.js
+++ script/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAhjjDKhZATV5Odh7rWjnp09glrSZ43Cy0",
+    authDomain: "rd-sem-final.firebaseapp.com",
+    projectId: "rd-sem-final",
+    storageBucket: "rd-sem-final.appspot.com",
+    messagingSenderId: "538172461184",
+    appId: "1:538172461184:web:e812a005825b62c2849665",
+    measurementId: "G-QLMYFHNQ4D"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
