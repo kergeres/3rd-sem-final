7a9dfc12ab420fd8960119d93ded3d31314a6bdb
WiktorrWie <you@example.com>
2020-12-07 10:13:37 +0100

    connected js with html, added js for collapsable home sections, changed styles

--- script/main.js
+++ script/main.js
@@ -3,28 +3,27 @@
 
 
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyAhjjDKhZATV5Odh7rWjnp09glrSZ43Cy0",
-    authDomain: "rd-sem-final.firebaseapp.com",
-    projectId: "rd-sem-final",
-    storageBucket: "rd-sem-final.appspot.com",
-    messagingSenderId: "538172461184",
-    appId: "1:538172461184:web:e812a005825b62c2849665",
-    measurementId: "G-QLMYFHNQ4D"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+   apiKey: "AIzaSyAhjjDKhZATV5Odh7rWjnp09glrSZ43Cy0",
+   authDomain: "rd-sem-final.firebaseapp.com",
+   projectId: "rd-sem-final",
+   storageBucket: "rd-sem-final.appspot.com",
+   messagingSenderId: "538172461184",
+   appId: "1:538172461184:web:e812a005825b62c2849665",
+   measurementId: "G-QLMYFHNQ4D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  const auth = firebase.auth();
-
-  
+const auth = firebase.auth();
 
 
-function signIn ()
-{
+
+
+function signIn() {
    let email = document.querySelector("#signUpEmail").value;
    let password = document.querySelector("#signUpPassw").value;
 
@@ -32,18 +31,32 @@ function signIn ()
    alert("User Logged in");
 }
 
-function signOut ()
-{
+function signOut() {
    auth.signOut();
    alert("User Signed out");
 }
 
-function signUp()
-{
+function signUp() {
    let email = document.querySelector("#signUpEmail").value;
    let password = document.querySelector("#signUpPassw").value;
 
    auth.createUserWithEmailAndPassword(email, password);
    alert("User Created");
-  
+
+   // home //
+
+   var coll = document.getElementsByClassName("collapsible");
+   var i;
+
+   for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+         this.classList.toggle("active");
+         var content = this.nextElementSibling;
+         if (content.style.display === "block") {
+            content.style.display = "none";
+         } else {
+            content.style.display = "block";
+         }
+      });
+   }
 }
