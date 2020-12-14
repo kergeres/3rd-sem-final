56372c63f57d4092a9bbc656466c16a6a5d5414f
WiktorrWie <you@example.com>
2020-12-14 10:53:08 +0100

    automatic opening sections working! :D

--- script/main.js
+++ script/main.js
@@ -532,10 +532,21 @@ function submitThis() {
 }
 
 function addBuckets() {
-
+  document.getElementById("menu-btn").checked = false;
+  document.getElementById('addBuckets').click();
 }
 
+function events() {
+  document.getElementById("menu-btn").checked = false;
+  document.getElementById('events').click();
+}
 
-window.onload = function () {
-  document.getElementById('addBuckets').click();
+function history() {
+  document.getElementById("menu-btn").checked = false;
+  document.getElementById('history').click();
+}
+
+function map() {
+  document.getElementById("menu-btn").checked = false;
+  document.getElementById('map').click();
 }
